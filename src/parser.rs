e725a392a2a822f6b408be53d14b27d58eb39d20
akthrms <akt.hrms@gmail.com>
2022-01-24 02:55:33 +0900

    update

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,24 @@
+use nom::IResult;
+
 enum Filter {
     Field(String, Box<Filter>),
     Index(isize, Box<Filter>),
     Null,
+}
+
+fn parse_filter(input: &str) -> IResult<&str, Filter> {
+    unimplemented!();
+}
+
+fn parse_field(input: &str) -> IResult<&str, Filter> {
+    unimplemented!();
+}
+
+fn parse_index(input: &str) -> IResult<&str, Filter> {
+    unimplemented!();
+}
+
+fn parse_null(input: &str) -> IResult<&str, Filter> {
+    let filter = Filter::Null;
+    Ok((input, filter))
 }
