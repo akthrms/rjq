9bc7d44d3fe4e5a763600dff0890a56ffb4d879b
akthrms <akt.hrms@gmail.com>
2022-01-24 03:04:37 +0900

    update

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,7 @@
 use nom::bytes::complete::tag;
 use nom::character::complete::digit1;
-use nom::IResult;
 use nom::sequence::{delimited, tuple};
+use nom::IResult;
 
 enum Filter {
     Field(String, Box<Filter>),
@@ -18,8 +18,8 @@ fn parse_field(input: &str) -> IResult<&str, Filter> {
 }
 
 fn parse_index(input: &str) -> IResult<&str, Filter> {
-    let (input, (digit, filter)) =
-        tuple((delimited(tag("["), digit1, tag("]")), parse_filter))(input)?;
+    let parse_digit = delimited(tag("["), digit1, tag("]"));
+    let (input, (digit, filter)) = tuple((parse_digit, parse_filter))(input)?;
     let filter = Filter::Index(digit.parse().unwrap(), Box::new(filter));
     Ok((input, filter))
 }
