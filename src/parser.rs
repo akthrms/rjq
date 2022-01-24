82f8f0accf740f700400953edba1751eb0230541
akthrms <akt.hrms@gmail.com>
2022-01-24 21:46:40 +0900

    update

--- src/parser.rs
+++ src/parser.rs
@@ -1,20 +1,20 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{alphanumeric1, digit1};
-use nom::combinator::recognize;
+use nom::combinator::{eof, recognize};
 use nom::multi::many1;
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
 
 enum Filter {
     Field(String, Box<Filter>),
-    Index(isize, Box<Filter>),
+    Index(usize, Box<Filter>),
     Null,
 }
 
 fn parse_filter(input: &str) -> IResult<&str, Filter> {
     let parse_filters = alt((parse_field, parse_index, parse_null));
-    let (input, (_, filter)) = tuple((tag("."), parse_filters))(input)?;
+    let (input, (_, filter, _)) = tuple((tag("."), parse_filters, eof))(input)?;
     Ok((input, filter))
 }
 
