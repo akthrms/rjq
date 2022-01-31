db63bd76e9681ea882f857b4430a14c75414a3d4
akthrms <akt.hrms@gmail.com>
2022-02-01 01:04:26 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn rjq(json_string: String, query_string: String) -> Result<String, String> {
+fn rjq(json_string: &str, query_string: &str) -> Result<String, String> {
     let value = serde_json::to_value(json_string).expect("invalid json format");
-    let query = parse_query(query_string.as_str())?;
+    let query = parse_query(query_string)?;
     execute_query(query, value).map(|value| value.to_string())
 }
